ee9d613a5c00bc83da806772e11f7236dc6d51ad
1kert <kertmand1@gmail.com>
2024-10-03 18:17:06 +0300

    login init

--- code/src/LoginPage.tsx
+++ code/src/LoginPage.tsx
@@ -1,10 +1,18 @@
+import Dollar from "./assets/Dollar sign.png"
+
 function LoginPage() {
     return (
-        <>
-            {Array(20).fill(1).map((x) => 
-                <p className="text-8xl">bruh</p>
-            )}
-        </>
+        <div className="p-16">
+            <div className="flex flex-col items-center gap-4">
+                <img src={Dollar} className="size-16" />
+                <h1 className="text-onContainerBlue text-4xl font-bold">SplitWise</h1>
+            </div>
+            <div className="gap-4 flex flex-col w-[325px] mt-12">
+                <input type="email" placeholder="Email" className="p-3.5 text-md w-full rounded-md bg-[#F1F4FF] border-2 border-solid border-onContainerBlue"/>
+                <input type="password" placeholder="Password" className="p-3.5 text-md w-full rounded-md bg-[#F1F4FF] focus:border-2 border-solid border-onContainerBlue"/>
+            </div>
+            <input type="button" value="Sign in" className="bg-onContainerBlue w-full py-3.5 text-xl text-white rounded-md mt-12" />
+        </div>
     )
 }
 
